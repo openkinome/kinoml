9bfbfdf619e713cfe09320c9bf16e17b462e697f
Jaime RGP <jaime.rogue@gmail.com>
2019-10-18 12:34:52 +0200

    Try to avoid some code duplication

--- devtools/scripts/install_miniconda.sh
+++ devtools/scripts/install_miniconda.sh
@@ -35,5 +35,14 @@ conda config --set always_yes yes
 conda install conda conda-build jinja2 anaconda-client
 conda update --quiet --all
 
+cat << "EOF" > ~/.minicondarc
+eval "$($HOME/miniconda/bin/conda shell.bash hook)"
+if [ -d $HOME/miniconda/envs/test ]; then
+    conda activate test
+else
+    conda activate
+fi
+EOF
+
 # Restore original directory
 popd
