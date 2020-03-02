62cd8e383d7abb1ebca1bdee119c1e76361873ad
Jaime RGP <jaime.rogue@gmail.com>
2020-03-02 11:34:00 +0100

    Black

--- kinoml/__init__.py
+++ kinoml/__init__.py
@@ -5,7 +5,9 @@ Machine Learning for kinase modeling
 
 # Handle versioneer
 from ._version import get_versions
+
 versions = get_versions()
-__version__ = versions['version']
-__git_revision__ = versions['full-revisionid']
+__version__ = versions["version"]
+__git_revision__ = versions["full-revisionid"]
 del get_versions, versions
+
