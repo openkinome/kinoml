d730eb7a06dcccac3a1f7efe1d9ca5fd38ea4ad5
Jaime RGP <jaime.rogue@gmail.com>
2020-03-02 13:11:34 +0100

    Add working prototype for mkdocs documentation

---
+++ docs/api.md
@@ -0,0 +1,6 @@
+# API documentation
+
+<!-- Examples: mkdocstrings requires paths to MODULES -->
+
+::: kinoml.smth
+::: kinoml.tests.test_kinoml
--- kinoml/__init__.py
+++ kinoml/__init__.py
@@ -5,7 +5,25 @@ Machine Learning for kinase modeling
 
 # Handle versioneer
 from ._version import get_versions
+
 versions = get_versions()
-__version__ = versions['version']
-__git_revision__ = versions['full-revisionid']
+__version__ = versions["version"]
+__git_revision__ = versions["full-revisionid"]
 del get_versions, versions
+
+
+def function(arg1, kwarg=None):
+    """
+    Example function
+
+    Parameters:
+        arg1:
+            Yeah
+        kwarg:
+            None
+
+    Returns:
+        smth:
+            Something
+    """
+    pass
