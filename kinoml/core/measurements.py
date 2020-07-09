ae08515786e5f60915aac8be4492c9ef4c53bfbd
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2020-07-09 22:58:36 +0200

    Add log10 variants

--- kinoml/core/measurements.py
+++ kinoml/core/measurements.py
@@ -51,7 +51,7 @@ class BaseMeasurement:
         return self._errors
 
     @classmethod
-    def observation_model(cls, backend="pytorch"):
+    def observation_model(cls, backend="pytorch", log10=False):
         """
         The observation_model function must be defined Measurement type, in the appropriate
         subclass. It dispatches to underlying static methods, suffixed by the
@@ -66,14 +66,21 @@ class BaseMeasurement:
         - `values`
         - `errors`
         """
-        return cls._observation_model(backend=backend)
+        return cls._observation_model(backend=backend, log10=log10)
 
     @classmethod
-    def _observation_model(cls, backend="pytorch", type_=None):
+    def _observation_model(cls, backend="pytorch", log10=False, type_=None):
         assert backend in ("pytorch", "tensorflow"), f"Backend {backend} is not supported!"
+        if log10:
+            return getattr(cls, f"_observation_model_log10_{backend}")
         return getattr(cls, f"_observation_model_{backend}")
 
-    def _observation_model_pytorch(self, **kwargs):
+    @staticmethod
+    def _observation_model_pytorch(*args, **kwargs):
+        raise NotImplementedError("Implement in your subclass!")
+
+    @staticmethod
+    def _observation_model_log10_pytorch(*args, **kwargs):
         raise NotImplementedError("Implement in your subclass!")
 
     def sanity_checks(self):
@@ -94,76 +101,72 @@ class BaseMeasurement:
 
 class PercentageDisplacementMeasurement(BaseMeasurement):
 
-    """
+    r"""
     Measurement where the value(s) must be percentage(s) of displacement.
+
+    For the percent displacement measurements available from KinomeScan, we make the assumption (see JDC's notes) that
+
+    $$
+    D([I]) \approx \frac{1}{1 + \frac{K_d}{[I]}}
+    $$
+
+    For KinomeSCAN assays, all assays are usually performed at a single molar concentration, $ [I] \sim 1 \mu M $.
+
+    We therefore define the following function:
+
+    $$
+    \mathbf{F}_{KinomeScan}(\Delta g, [I]) = \frac{1}{1 + \frac{exp[\Delta g] * 1[M]}{[I]}}.
+    $$
     """
 
     def sanity_checks(self):
         super().sanity_checks()
         assert (0 <= self.values <= 100).all(), "One or more values are not in [0, 100]"
 
-    @classmethod
-    def observation_model(cls, backend="pytorch"):
-        r"""
-        For the percent displacement measurements available from KinomeScan, we make the assumption (see JDC's notes) that
-
-        $$
-        D([I]) \approx \frac{1}{1 + \frac{K_d}{[I]}}
-        $$
-
-        For KinomeSCAN assays, all assays are usually performed at a single molar concentration, $ [I] \sim 1 \mu M $.
-
-        We therefore define the following function:
-
-        $$
-        \mathbf{F}_{KinomeScan}(\Delta g, [I]) = \frac{1}{1 + \frac{exp[\Delta g] * 1[M]}{[I]}}.
-        $$
-        """
-        return cls._observation_model(backend=backend)
-
     @staticmethod
     def _observation_model_pytorch(values, inhibitor_conc=1e-6, **kwargs):
         import torch
 
-        # values = torch.from_numpy(values)
         return 100 / (1 + torch.exp(values) / inhibitor_conc)
+
+    @staticmethod
+    def _observation_model_log10_pytorch(*args, **kwargs):
+        import torch
+
+        return -torch.log10(
+            PercentageDisplacementMeasurement._observation_model_pytorch(*args, **kwargs)
+        )
 
 
 class IC50Measurement(BaseMeasurement):
 
-    """
+    r"""
     Measurement where the value(s) come from IC50 experiments
+
+    We use the Cheng Prusoff equation here.
+
+    The [Cheng Prusoff](https://en.wikipedia.org/wiki/IC50#Cheng_Prusoff_equation) equation states the following relationship
+
+    \begin{equation}
+    K_i = \frac{IC50}{1+\frac{[S]}{K_m}}
+    \end{equation}
+
+    We make the following assumptions here
+    1. $[S] = K_m$
+    2. $K_i \approx K_d$
+
+    In the future, we will relax these assumptions.
+
+    Under these assumptions, the Cheng-Prusoff equation becomes
+    $$
+    IC50 \approx 2 * K_d
+    $$
+
+    We define the following function
+    $$
+    \mathbf{F}_{IC50}(\Delta g) = 2 * \mathbf{F}_{K_d}(\Delta g) = 2 * exp[-\Delta g] * 1[M]
+    $$
     """
-
-    @classmethod
-    def observation_model(cls, backend="pytorch"):
-        r"""
-        We use the Cheng Prusoff equation here.
-
-        The [Cheng Prusoff](https://en.wikipedia.org/wiki/IC50#Cheng_Prusoff_equation) equation states the following relationship
-
-        \begin{equation}
-        K_i = \frac{IC50}{1+\frac{[S]}{K_m}}
-        \end{equation}
-
-        We make the following assumptions here
-        1. $[S] = K_m$
-        2. $K_i \approx K_d$
-
-        In the future, we will relax these assumptions.
-
-        Under these assumptions, the Cheng-Prusoff equation becomes
-        $$
-        IC50 \approx 2 * K_d
-        $$
-
-        We define the following function
-        $$
-        \mathbf{F}_{IC50}(\Delta g) = 2 * \mathbf{F}_{K_d}(\Delta g) = 2 * exp[-\Delta g] * 1[M]
-        $$
-
-        """
-        return cls._observation_model(backend=backend)
 
     @staticmethod
     def _observation_model_pytorch(
@@ -171,55 +174,61 @@ class IC50Measurement(BaseMeasurement):
     ):
         import torch
 
-        # values = torch.from_numpy(values.reshape((-1, 1)))
         return (1 + substrate_conc / michaelis_constant) * torch.exp(values) * inhibitor_conc
+
+    @staticmethod
+    def _observation_model_log10_pytorch(*args, **kwargs):
+        import torch
+
+        return -torch.log10(IC50Measurement._observation_model_pytorch(*args, **kwargs))
 
 
 class KiMeasurement(BaseMeasurement):
 
-    """
+    r"""
     Measurement where the value(s) come from K_i_ experiments
+
+    We make the assumption that $K_i \approx K_d$ and therefore $\mathbf{F}_{K_i} = \mathbf{F}_{K_d}$.
     """
 
-    @classmethod
-    def observation_model(cls, backend="pytorch"):
-        r"""
-        We make the assumption that $K_i \approx K_d$ and therefore $\mathbf{F}_{K_i} = \mathbf{F}_{K_d}$.
-        """
-        return cls._observation_model(backend=backend)
-
-    def _observation_model_pytorch(self, values, inhibitor_conc=1, **kwargs):
+    @staticmethod
+    def _observation_model_pytorch(values, inhibitor_conc=1, **kwargs):
         import torch
 
-        # values = torch.from_numpy(values.reshape((-1, 1)))
         return torch.exp(values) * inhibitor_conc
+
+    @staticmethod
+    def _observation_model_log10_pytorch(*args, **kwargs):
+        import torch
+
+        return -torch.log10(KiMeasurement._observation_model_pytorch(*args, **kwargs))
 
 
 class KdMeasurement(BaseMeasurement):
 
-    """
+    r"""
     Measurement where the value(s) come from Kd experiments
+
+    We define the following physics-based function
+    $$
+    \mathbf{F}_{K_d}(\Delta g) = exp[-\Delta g] * 1[M].
+    $$
+
+    If we have measurements at different concentrations $I$ (unit [M]) , then the function can further be defined as
+
+    $$
+    \mathbf{F}_{K_d}(\Delta g, I) = exp[-\Delta g] * I[M].
+        $$
     """
 
-    @classmethod
-    def observation_model(cls, backend="pytorch"):
-        r"""
-        We define the following physics-based function
-        $$
-        \mathbf{F}_{K_d}(\Delta g) = exp[-\Delta g] * 1[M].
-        $$
-
-        If we have measurements at different concentrations $I$ (unit [M]) , then the function can further be defined as
-
-        $$
-        \mathbf{F}_{K_d}(\Delta g, I) = exp[-\Delta g] * I[M].
-        $$
-
-        """
-        return cls._observation_model(backend=backend)
-
-    def _observation_model_pytorch(self, values, inhibitor_conc=1, **kwargs):
+    @staticmethod
+    def _observation_model_pytorch(values, inhibitor_conc=1, **kwargs):
         import torch
 
-        # values = torch.from_numpy(values.reshape((-1, 1)))
         return torch.exp(values) * inhibitor_conc
+
+    @staticmethod
+    def _observation_model_log10_pytorch(*args, **kwargs):
+        import torch
+
+        return -torch.log10(KdMeasurement._observation_model_pytorch(*args, **kwargs))
