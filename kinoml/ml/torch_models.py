d30a40590b9bd543086ecbc6a9073f0606fa2f92
Talia Kimber <talia.kimber@gmail.com>
2020-12-04 15:41:57 +0100

    add convolutional network.

--- kinoml/ml/torch_models.py
+++ kinoml/ml/torch_models.py
@@ -5,7 +5,18 @@ import torch.nn.functional as F
 
 class NeuralNetworkRegression(nn.Module):
     """
-    Builds a Pytorch model (a Dense Neural Network) and a feed-forward pass
+    Builds a Pytorch model (a vanilla neural network) and a feed-forward pass.
+
+    Parameters
+    ----------
+        input_size : int, default=1024
+                Dimension of the input vector.
+        hidden_size : int, default=100
+                Number of units in the hidden layer.
+        output_size : int, default=1
+                Size of the last unit, representing delta_g_over_kt in our setting.
+        _activation : torch function, default: relu
+                The activation function used in the hidden (only!) layer of the network.
     """
 
     def __init__(self, input_size=1024, hidden_size=100, output_size=1, activation=F.relu):
@@ -23,18 +34,18 @@ class NeuralNetworkRegression(nn.Module):
 
     def forward(self, x):
         """
-        Defines the foward pass for a given input 'x'
+        Defines the foward pass for a given input 'x'.
         """
-        x = self._activation(self.fully_connected_1(x))  # Activations are ReLU
+        x = self._activation(self.fully_connected_1(x))
         return self.fully_connected_out(x)
 
 
-class DNN(nn.Module):
+class DenseNeuralNetworkRegression(nn.Module):
     """
-    Builds a Dense Neural Network and a feed-forward pass
+    Builds a Dense Neural Network and a feed-forward pass.
 
     Parameters:
-        input_dim: Expected shape of the input data
+        # TODO
     """
 
     def __init__(self, input_dim: int = 512):
@@ -65,36 +76,50 @@ class DNN(nn.Module):
         return torch.sigmoid(x)
 
 
-class CNN(nn.Module):
+class ConvolutionNeuralNetworkRegression(nn.Module):
     """
-    Builds a Convolutional Neural Network and a feed-forward pass
+    Builds a Convolutional Neural Network and a feed-forward pass.
 
-    Parameters:
-        input_shape: Expected shape of the input data
-
-    !!! todo
-        Provide keyword arguments for hardcoded numbers in `__init__`
+    Parameters
+    ----------
+        nb_char : int, default=53
+                Expected number of possible characters
+                For SMILES characters, we assume 53.
+        max_length : int, default=256
+                Maximum length of SMILES, set to 256.
+        embedding_dim : int, default=200
+                Dimension of the embedding after convolution.
+        kernel_size : int, default=10
+                Size of the kernel for the convolution.
+        hidden_size : int, default=100
+                Number of units in the hidden layer.
+        output_size : int, default=1
+                Size of the last unit, representing delta_g_over_kt in our setting.
+        _activation : torch function, default: relu
+                The activation function used in the hidden (only!) layer of the network.
     """
 
-    def __init__(self, input_shape: int = 53):
-        super(CNN, self).__init__()
-        self.input_shape = input_shape
-        self.conv = nn.Conv1d(in_channels=self.input_shape, out_channels=100, kernel_size=10)
-        self.dropout = nn.Dropout(0.2)
-        self.fc1 = nn.Linear(22 * 100, 64)
-        self.fc2 = nn.Linear(64, 32)
-        self.fc3 = nn.Linear(32, 1)
+    def __init__(self, nb_char=53, max_length=256, embedding_dim=300, kernel_size=10, hidden_size=100, output_size=1, activation=F.relu):
+        super(ConvolutionNeuralNetworkRegression, self).__init__()
+
+        self.nb_char = nb_char
+        self.max_length = max_length
+        self.embedding_dim = embedding_dim
+        self.kernel_size = kernel_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self._activation = activation
+
+        self.convolution = nn.Conv1d(in_channels=self.nb_char, out_channels=self.embedding_dim, kernel_size=self.kernel_size)
+        self.temp = (self.max_length - self.kernel_size + 1)  * self.embedding_dim
+        self.fully_connected_1 = nn.Linear(self.temp, self.hidden_size)
+        self.fully_connected_out = nn.Linear(self.hidden_size, self.output_size)
 
     def forward(self, x):
         """
         Defines the foward pass for a given input 'x'
         """
-        x = self.conv(x)
-        x = F.relu(x)
-        x = F.max_pool1d(x, 4)
+        x = self._activation(self.convolution(x))
         x = torch.flatten(x, 1)
-        x = self.dropout(x)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return torch.sigmoid(x)
+        x = self._activation(self.fully_connected_1(x))
+        return self.fully_connected_out(x)
