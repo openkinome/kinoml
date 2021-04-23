aea02e1aee2657611a05838e703d0a7871fd20d6
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2021-04-23 14:50:35 +0200

    add tests for fragile 3rd party imports

--- kinoml/tests/test_kinoml.py
+++ kinoml/tests/test_kinoml.py
@@ -12,3 +12,16 @@ def test_kinoml_imported():
     Sample test, will always pass so long as import statement worked
     """
     assert "kinoml" in sys.modules
+
+
+def test_3rdparty_imports():
+    """
+    Some packages can be tricky to install. Make sure we can import them.
+    """
+    import torch  # pylint: disable=unused-import
+
+    assert "torch" in sys.modules
+
+    import torch_geometric  # pylint: disable=unused-import
+
+    assert "torch_geometric" in sys.modules
