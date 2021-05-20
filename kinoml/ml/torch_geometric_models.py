b6ffd728ad8209f4c809f5c0598f752f2bd8e919
t-kimber <talia.kimber@gmail.com>
2021-05-20 10:21:07 +0200

    GCN edits

--- kinoml/ml/torch_geometric_models.py
+++ kinoml/ml/torch_geometric_models.py
@@ -2,8 +2,9 @@
 Implementation of some Deep Neural Networks in Pytorch using Pytorch Geometric.
 """
 
+from torch.nn import Linear
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GCNConv, global_mean_pool
 from .torch_models import _BaseModule
 
 
@@ -13,10 +14,12 @@ class GraphConvolutionNeuralNetwork(_BaseModule):
 
     Parameters
     ----------
-    nb_nodes_features : int
+    input_shape : int
         Number of features per node in the graph.
     embedding_shape : int, default=100
         Dimension of latent vector.
+    hidden_shape : int, default=50
+        Dimension of the hidden shape.
     output_shape : int, default=1
         Size of the last unit, representing delta_g_over_kt in our setting.
     _activation : torch function, default=relu
@@ -30,18 +33,24 @@ class GraphConvolutionNeuralNetwork(_BaseModule):
         # Take the first batch [0]
         return input_sample[0].num_node_features
 
-    def __init__(self, input_shape, embedding_shape=100, output_shape=1, activation=F.relu):
+    def __init__(self, input_shape, embedding_shape=100, hidden_shape=50, output_shape=1, activation=F.relu):
         super().__init__()
         self.input_shape = input_shape
         self.embedding_shape = embedding_shape
+        self.hidden_shape = hidden_shape
         self.output_shape = output_shape
         self._activation = activation
 
         self.GraphConvLayer1 = GCNConv(self.input_shape, self.embedding_shape)
-        self.GraphConvLayer2 = GCNConv(self.embedding_shape, self.output_shape)
+        self.GraphConvLayer2 = GCNConv(self.embedding_shape, self.hidden_shape)
+
+        self.linear = Linear(self.hidden_shape, self.output_shape)
 
     def forward(self, data):
         data = data[0]  # get the first one only?
-        x, edge_index = data.x.float(), data.edge_index.long()
+        x, edge_index, batch = data.x.float(), data.edge_index.long(), data.batch
         x = self._activation(self.GraphConvLayer1(x, edge_index))
-        return self.GraphConvLayer2(x, edge_index)
+        x = self._activation(self.GraphConvLayer2(x, edge_index))
+        x = global_mean_pool(x, batch)
+        return self.linear(x)
+
