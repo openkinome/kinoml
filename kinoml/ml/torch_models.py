c3039250a4cc7974f496f333957d13ca5590ef2a
t-kimber <talia.kimber@gmail.com>
2021-08-18 10:03:07 +0200

    Update docs and remove print statements.

--- kinoml/ml/torch_models.py
+++ kinoml/ml/torch_models.py
@@ -63,7 +63,6 @@ class NeuralNetworkRegression(_BaseModule):
         """
         x = x[0]
         x = x.float()
-        print(f"{x.shape = }")
         x = self._activation(self.fully_connected_1(x))
         return self.fully_connected_out(x)
 
@@ -263,7 +262,6 @@ class ConvolutionNeuralNetworkRegressionKinaseInformed(_BaseModule):
         )
         self.temp_ligand = (self.input_shape[0][1] - self.kernel_shape + 1) * self.embedding_shape
 
-
         # Convolution on protein
         self.convolution_protein = nn.Conv1d(
             in_channels=self.input_shape[1][0],
@@ -280,8 +278,6 @@ class ConvolutionNeuralNetworkRegressionKinaseInformed(_BaseModule):
         Defines the foward pass for given an input composed of two entities: ligand and protein.
         """
         x_ligand, x_protein = x[0], x[1]
-        print(f"{x_ligand.shape = }")
-        print(f"{x_protein.shape = }")
 
         x_lig = self._activation(self.convolution_ligand(x_ligand.float()))
         x_prot = self._activation(self.convolution_protein(x_protein.float()))
@@ -303,8 +299,10 @@ class NeuralNetworkRegressionKinaseInformed(_BaseModule):
    ----------
    input_shape : tuple
        Dimension of input tensors, for the ligand and the protein.
-   embedding_shape : int, default=200
-       Dimension of the embedding after convolution.
+   embedding_shape_ligand : int, default=300
+       Dimension of the embedding for the ligand.
+   embedding_shape_protein : int, default=10
+       Dimension of the embedding for the protein.
    kernel_shape : int, default=10
        Size of the kernel for the convolution.
    hidden_shape : int, default=100
@@ -319,7 +317,8 @@ class NeuralNetworkRegressionKinaseInformed(_BaseModule):
    def __init__(
        self,
        input_shape,
-       embedding_shape=300,
+       embedding_shape_ligand=300,
+       embedding_shape_protein=10,
        kernel_shape=10,
        hidden_shape=100,
        output_shape=1,
@@ -328,19 +327,20 @@ class NeuralNetworkRegressionKinaseInformed(_BaseModule):
        super().__init__()
 
        self.input_shape = input_shape
-
-       self.embedding_shape = embedding_shape
+       self.embedding_shape_ligand = embedding_shape_ligand
+       self.embedding_shape_protein = embedding_shape_protein
        self.kernel_shape = kernel_shape
        self.hidden_shape = hidden_shape
        self.output_shape = output_shape
        self._activation = activation
 
        # Fully connected layer on ligand
-       self.fully_connected_ligand = nn.Linear(self.input_shape[0], 300)
-       self.fully_connected_protein = nn.Linear(self.input_shape[1], 10)
+       self.fully_connected_ligand = nn.Linear(self.input_shape[0], self.embedding_shape_ligand)
+       # Fully connected layer on protein
+       self.fully_connected_protein = nn.Linear(self.input_shape[1], self.embedding_shape_protein)
 
        # Ligand - protein
-       self.ligand_protein = 310
+       self.ligand_protein = self.embedding_shape_ligand + self.embedding_shape_protein
 
        # Output
        self.fully_connected_1 = nn.Linear(self.ligand_protein, self.hidden_shape)
@@ -351,17 +351,11 @@ class NeuralNetworkRegressionKinaseInformed(_BaseModule):
        Defines the foward pass for given an input composed of two entities: ligand and protein.
        """
        x_ligand, x_protein = x[0], x[1]
-       print(f"{x_ligand.shape = }")
-       print(f"{x_protein.shape = }")
 
        x_lig = self._activation(self.fully_connected_ligand(x_ligand.float()))
        x_prot = self._activation(self.fully_connected_protein(x_protein.float()))
 
-       # x_lig = torch.flatten(x_lig, 1)
-       # x_prot = torch.flatten(x_prot, 1)
-
        x = torch.cat((x_lig, x_prot), dim=1)
-
        x = self._activation(self.fully_connected_1(x))
 
        return self.fully_connected_out(x)
