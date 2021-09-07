37a8b7020278ddf12fc94204842bfd92529e9a58
David Schaller <schaller.david@yahoo.de>
2021-09-07 10:02:27 +0200

    implement OEPositDockingFeaturizer and tests

--- kinoml/features/complexes.py
+++ kinoml/features/complexes.py
@@ -157,7 +157,7 @@ class OEHybridDockingFeaturizer(OEBaseModelingFeaturizer):
        (see below)
      - removing everything but protein, water and ligand of interest
      - protonation at pH 7.4
-     - perform hybrid docking
+     - perform Hybrid docking
 
     The protein component of each system must have a `pdb_id` or a `path` attribute specifying
     the complex structure to prepare.
@@ -172,7 +172,7 @@ class OEHybridDockingFeaturizer(OEBaseModelingFeaturizer):
        output file name.
      - `chain_id`: A string specifying which chain should be used.
      - `alternate_location`: A string specifying which alternate location should be used.
-     - `expo_id`: A string specifying the ligand to use during hybrid docking. This is especially
+     - `expo_id`: A string specifying the ligand to use during Hybrid docking. This is especially
        useful if multiple ligands are present in a PDB structure.
      - `uniprot_id`: A string specifying the UniProt ID that will be used to fetch the amino acid
        sequence from UniProt, which will be used for modeling the protein. This will supersede the
@@ -319,7 +319,7 @@ class OEFredDockingFeaturizer(OEBaseModelingFeaturizer):
        (see below)
      - removing everything but protein, water and ligand of interest
      - protonation at pH 7.4
-     - perform fred docking
+     - perform Fred docking
 
     The protein component of each system must have a `pdb_id` or a `path` as well as a
     `pocket_resids` attribute specifying the structure to prepare and the binding pocket to dock
@@ -378,7 +378,7 @@ class OEFredDockingFeaturizer(OEBaseModelingFeaturizer):
 
     def _featurize_one(self, system: ProteinLigandComplex) -> universe:
         """
-        Prepare a protein structure and dock a ligand using OpenEye's Hybrid method.
+        Prepare a protein structure and dock a ligand using OpenEye's Fred method.
 
         Parameters
         ----------
@@ -441,6 +441,168 @@ class OEFredDockingFeaturizer(OEBaseModelingFeaturizer):
 
         logging.debug("Performing docking ...")
         docking_pose = fred_docking(
+            design_unit,
+            [read_smiles(system.ligand.to_smiles())],
+            pKa_norm=self.pKa_norm
+        )[0]
+        # generate residue information
+        oechem.OEPerceiveResidues(docking_pose, oechem.OEPreserveResInfo_None)
+
+        logging.debug("Assembling components ...")
+        protein_ligand_complex = self._assemble_components(protein, solvent, docking_pose)
+
+        logging.debug("Updating pdb header ...")
+        protein_ligand_complex = self._update_pdb_header(
+            protein_ligand_complex,
+            protein_name=system.protein.name,
+            ligand_name=system.ligand.name,
+        )
+
+        logging.debug("Writing results ...")
+        file_path = self._write_results(
+            protein_ligand_complex,
+            "_".join([
+                system_dict["protein_name"],
+                system_dict["protein_pdb_id"] if system_dict["protein_pdb_id"]
+                else system_dict["protein_path"].stem,
+                f"chain{system_dict['protein_chain_id']}",
+                f"altloc{system_dict['protein_alternate_location']}"
+            ]),
+            system.ligand.name,
+        )
+
+        logging.debug("Generating new MDAnalysis universe ...")
+        structure = mda.Universe(file_path, in_memory=True)
+
+        if not self.output_dir:
+            logging.debug("Removing structure file ...")
+            file_path.unlink()
+
+        return structure
+
+
+class OEPositDockingFeaturizer(OEBaseModelingFeaturizer):
+    """
+    Given systems with exactly one protein and one ligand, dock the ligand into the prepared
+    protein structure with OpenEye's POSIT method:
+
+     - modeling missing loops
+     - building missing side chains
+     - mutations, if `uniprot_id` or `sequence` attribute is provided for the protein component
+       (see below)
+     - removing everything but protein, water and ligand of interest
+     - protonation at pH 7.4
+     - perform Posit docking
+
+    The protein component of each system must have a `pdb_id` or a `path` attribute specifying
+    the complex structure to prepare.
+
+     - `pdb_id`: A string specifying the PDB entry of interest, required if `path` not given.
+     - `path`: The path to the structure file, required if `pdb_id` not given.
+
+    Additionally, the protein component can have the following optional attributes to customize
+    the protein modeling:
+
+     - `name`: A string specifying the name of the protein, will be used for generating the
+       output file name.
+     - `chain_id`: A string specifying which chain should be used.
+     - `alternate_location`: A string specifying which alternate location should be used.
+     - `expo_id`: A string specifying the ligand to use during Posit docking. This is especially
+       useful if multiple ligands are present in a PDB structure.
+     - `uniprot_id`: A string specifying the UniProt ID that will be used to fetch the amino acid
+       sequence from UniProt, which will be used for modeling the protein. This will supersede the
+       sequence information given in the PDB header.
+     - `sequence`: An `AminoAcidSequence` object specifying the amino acid sequence that should be
+       used during modeling the protein. This will supersede a given `uniprot_id` and the sequence
+       information given in the PDB header.
+
+    The ligand component of each system must have a `to_smiles` method allowing access to the
+    molecular structure to dock, e.g. a Ligand object from `core.ligands` initiated as
+    Ligand.from_smiles("CCOCCC").
+
+    Additionally, the ligand component can have the following optional attributes:
+
+     - `name`: A string specifying the name of the ligand, will be used for generating the
+       output file name.
+
+    Parameters
+    ----------
+    loop_db: str
+        The path to the loop database used by OESpruce to model missing loops.
+    cache_dir: str, Path or None, default=None
+        Path to directory used for saving intermediate files. If None, default location
+        provided by `appdirs.user_cache_dir()` will be used.
+    output_dir: str, Path or None, default=None
+        Path to directory used for saving output files. If None, output structures will not be
+        saved.
+    pKa_norm: bool, default=True
+        Assign the predominant ionization state of the molecules to dock at pH ~7.4. If False,
+        the ionization state of the input molecules will be conserved.
+
+    Note
+    ----
+    The provided protein must have a co-crystallized ligand to allow docking with OpenEye's
+    Posit method.
+    """
+    from MDAnalysis.core import universe
+
+    def __init__(self, pKa_norm: bool = True, **kwargs):
+        super().__init__(**kwargs)
+        self.pKa_norm = pKa_norm
+
+    _SUPPORTED_TYPES = (ProteinLigandComplex,)
+
+    def _featurize_one(self, system: ProteinLigandComplex) -> universe:
+        """
+        Prepare a protein structure and dock a ligand using OpenEye's Posit method.
+
+        Parameters
+        ----------
+        system: ProteinLigandComplex
+            A system object holding a protein and a ligand component.
+
+        Returns
+        -------
+        : universe
+            An MDAnalysis universe of the featurized system.
+        """
+        import MDAnalysis as mda
+        from openeye import oechem, oedocking
+
+        from ..docking.OEDocking import pose_molecules
+        from ..modeling.OEModeling import read_smiles, read_molecules
+
+        logging.debug("Interpreting system ...")
+        system_dict = self._interpret_system(system)
+
+        logging.debug("Reading structure ...")
+        structure = read_molecules(system_dict["protein_path"])[0]
+
+        logging.debug("Preparing protein ligand complex ...")
+        design_unit = self._get_design_unit(
+            structure=structure,
+            chain_id=system_dict["protein_chain_id"],
+            alternate_location=system_dict["protein_alternate_location"],
+            has_ligand=True,
+            ligand_name=system_dict["protein_expo_id"],
+            model_loops_and_caps=False if system_dict["protein_sequence"] else True,
+        )  # if sequence is given model loops and caps separately later
+
+        logging.debug("Extracting design unit components ...")
+        protein, solvent, ligand = self._get_components(
+            design_unit, system_dict["protein_chain_id"]
+        )
+
+        if system_dict["protein_sequence"]:
+            protein = self._process_protein(protein, system_dict["protein_sequence"])
+            oechem.OEUpdateDesignUnit(design_unit, protein, oechem.OEDesignUnitComponents_Protein)
+
+        if not design_unit.HasReceptor():
+            logging.debug("Preparing receptor for docking ...")
+            oedocking.OEMakeReceptor(design_unit)
+
+        logging.debug("Performing docking ...")
+        docking_pose = pose_molecules(
             design_unit,
             [read_smiles(system.ligand.to_smiles())],
             pKa_norm=self.pKa_norm
--- kinoml/tests/features/test_complex.py
+++ kinoml/tests/features/test_complex.py
@@ -232,3 +232,79 @@ def test_OEFredDockingFeaturizer(
                 u = system.featurizations["OEFredDockingFeaturizer"].universe
                 n_ligand_atoms = u.select_atoms("resname LIG").atoms.n_atoms
                 assert n_ligand_atoms == expected_n_ligand_atoms
+
+
+@pytest.mark.parametrize(
+    "package, resource_list, pdb_id, uniprot_id, chain_id, alternate_location, expo_id, "
+    "smiles, expected_n_ligand_atoms",
+    [
+        (
+            "kinoml.data.proteins",
+            ["kinoml_tests_4f8o_spruce.loop_db"],
+            "4f8o",
+            "P31522",
+            "A",
+            "B",
+            "AES",
+            "c1cc(ccc1CCN)S(=O)(=O)F",
+            24
+        ),
+        (
+            "kinoml.data.proteins",
+            ["kinoml_tests_4f8o_spruce.loop_db", "4f8o.pdb"],
+            None,
+            None,
+            None,
+            None,
+            None,
+            "c1cc(ccc1CCN)S(=O)(=O)F",
+            24
+        ),
+    ],
+)
+def test_OEPositDockingFeaturizer(
+        package,
+        resource_list,
+        pdb_id,
+        uniprot_id,
+        chain_id,
+        alternate_location,
+        expo_id,
+        smiles,
+        expected_n_ligand_atoms
+):
+    """
+    Compare featurizer results to expected number of ligand atoms.
+    """
+    from kinoml.core.ligands import Ligand
+    from kinoml.core.proteins import BaseProtein
+    from kinoml.core.systems import ProteinLigandComplex
+    from kinoml.features.complexes import OEPositDockingFeaturizer
+
+    with resources.path(package, resource_list[0]) as loop_db:
+        featurizer = OEPositDockingFeaturizer(loop_db=loop_db)
+        ligand = Ligand.from_smiles(smiles=smiles, name="LIG")
+        base_protein = BaseProtein(name="PsaA")
+        if uniprot_id:
+            base_protein.uniprot_id = uniprot_id
+        if chain_id:
+            base_protein.chain_id = chain_id
+        if alternate_location:
+            base_protein.alternate_location = alternate_location
+        if expo_id:
+            base_protein.expo_id = expo_id
+        if pdb_id:
+            base_protein.pdb_id = pdb_id
+            system = ProteinLigandComplex([base_protein, ligand])
+            system = featurizer.featurize([system])[0]
+            u = system.featurizations["OEPositDockingFeaturizer"].universe
+            n_ligand_atoms = u.select_atoms("resname LIG").atoms.n_atoms
+            assert n_ligand_atoms == expected_n_ligand_atoms
+        else:
+            with resources.path(package, resource_list[1]) as path:
+                base_protein.path = path
+                system = ProteinLigandComplex([base_protein, ligand])
+                system = featurizer.featurize([system])[0]
+                u = system.featurizations["OEPositDockingFeaturizer"].universe
+                n_ligand_atoms = u.select_atoms("resname LIG").atoms.n_atoms
+                assert n_ligand_atoms == expected_n_ligand_atoms
