685a3b931fb2ccc2b12485edcb9892d1553b0da5
David Schaller <schaller.david@yahoo.de>
2021-11-04 12:19:29 +0100

    add function for downloading pdb structures

--- kinoml/tests/databases/test_pdb.py
+++ kinoml/tests/databases/test_pdb.py
@@ -4,8 +4,6 @@ Test pdb functionalities of `kinoml.databases`
 from contextlib import contextmanager
 import pytest
 
-from kinoml.databases.pdb import smiles_from_pdb
-
 
 @contextmanager
 def does_not_raise():
@@ -34,7 +32,36 @@ def does_not_raise():
 )
 def test_smiles_from_pdb(pdb_ids, expectation, smiles_list):
     """Compare results for expected SMILES."""
+    from kinoml.databases.pdb import smiles_from_pdb
+
     with expectation:
         ligands = smiles_from_pdb(pdb_ids)
         for pdb_id, smiles in zip(pdb_ids, smiles_list):
             assert ligands[pdb_id] == smiles
+
+
+@pytest.mark.parametrize(
+    "pdb_id, success",
+    [
+        (
+            "4YNE",  # PDB and CIF format available
+            True,
+        ),
+        (
+            "1BOS",  # only CIF format available
+            True,
+        ),
+        (
+            "XXXX",  # wrong code
+            False,
+        ),
+    ],
+)
+def test_download_pdb_structure(pdb_id, success):
+    """Try to download PDB structures."""
+    from tempfile import TemporaryDirectory
+
+    from kinoml.databases.pdb import download_pdb_structure
+
+    with TemporaryDirectory() as temporary_directory:
+        assert download_pdb_structure(pdb_id, temporary_directory) == success
