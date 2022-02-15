0fa1a89275de4a4d6d2de3b9b539d57e5957e5f8
David Schaller <schaller.david@yahoo.de>
2022-02-15 09:28:15 +0100

    fix guessing bonds of Maestro like element names

--- kinoml/modeling/MDAnalysisModeling.py
+++ kinoml/modeling/MDAnalysisModeling.py
@@ -9,7 +9,7 @@ from MDAnalysis.core.groups import AtomGroup, Residue
 logger = logging.getLogger(__name__)
 
 
-def read_molecule(path: Union[str, Path]) -> Universe:
+def read_molecule(path: Union[str, Path], guess_bonds: bool = True) -> Universe:
     """
     Read a molecule from a file. Uses Biopython to support reading of the CIF format.
 
@@ -17,13 +17,25 @@ def read_molecule(path: Union[str, Path]) -> Universe:
     ----------
     path: str, pathlib.Path
         Path to molecule file.
+    guess_bonds: bool, default=True
+        If bonds should be guessed by the van-der-Waals radius.
 
     Returns
     -------
     molecule: MDAnalysis.core.universe.Universe
         The MDAnalysis universe.
     """
+    import copy
+
     import MDAnalysis as mda
+    from MDAnalysis.topology.tables import vdwradii
+
+    # add Maestro-like atom names to vdwradii dictionary needed for guessing bonds
+    # e.g. Cl instead of CL
+    vdwradii_new = copy.deepcopy(vdwradii)
+    for key, value in vdwradii.items():
+        if len(key) == 2:
+            vdwradii_new[f"{key[0]}{key[1].lower()}"] = value
 
     path = str(Path(path).expanduser().resolve())
     suffix = path.split(".")[-1]
@@ -37,9 +49,13 @@ def read_molecule(path: Union[str, Path]) -> Universe:
             io = PDBIO()
             io.set_structure(structure)
             io.save(tempfile.name)
-            molecule = mda.Universe(tempfile.name, in_memory=True, dt=0, guess_bonds=True)
+            molecule = mda.Universe(
+                tempfile.name, in_memory=True, dt=0, vdwradii=vdwradii_new, guess_bonds=guess_bonds
+            )
     else:
-        molecule = mda.Universe(path, in_memory=True, dt=0, guess_bonds=True)
+        molecule = mda.Universe(
+            path, in_memory=True, dt=0, vdwradii=vdwradii_new, guess_bonds=guess_bonds
+        )
 
     return molecule
 
