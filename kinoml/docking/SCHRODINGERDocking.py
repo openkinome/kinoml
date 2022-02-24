ad9a19d6a154fe98dbb04c97c5f6e85ed82a12d0
David Schaller <schaller.david@yahoo.de>
2022-02-24 12:17:44 +0100

    schrodinger docking featurizer [WIP]

---
+++ kinoml/docking/SCHRODINGERDocking.py
@@ -0,0 +1,141 @@
+import logging
+from pathlib import Path
+import subprocess
+from tempfile import NamedTemporaryFile
+from typing import List, Union
+
+
+logger = logging.getLogger(__name__)
+
+
+def run_glide(
+        schrodinger_directory: Union[Path, str],
+        input_file_mae: Union[Path, str],
+        output_file_sdf: Union[Path, str],
+        ligand_resname: str,
+        mols_smiles: Union[List[str]],
+        n_poses: int = 1,
+        mols_names: Union[List[str], None] = None,
+        shape_restrain: bool = True,
+        macrocyles: bool = False,
+        precision: str = "XP",
+):
+    from rdkit import Chem
+    from rdkit.Chem import AllChem
+
+    from ..modeling.MDAnalysisModeling import read_molecule
+    from ..modeling.SCHRODINGERModeling import mae_to_pdb
+
+    if precision not in ["HTVS", "SP", "XP"]:
+        raise ValueError(
+            f"Only 'HTVS', 'SP', 'XP' are allowed for precision, you provided {precision}!"
+        )
+
+    schrodinger_directory = Path(schrodinger_directory).resolve()
+    input_file_mae = Path(input_file_mae).resolve()
+    with NamedTemporaryFile(mode="w", suffix=".mae") as protein_file_mae, \
+            NamedTemporaryFile(mode="w", suffix=".pdb") as ligand_file_pdb, \
+            NamedTemporaryFile(mode="w", suffix=".sdf") as mols_file_sdf, \
+            NamedTemporaryFile(mode="w", suffix=".in") as grid_input_file, \
+            NamedTemporaryFile(mode="w", suffix=".in") as docking_input_file:
+
+        logger.debug("Selecting and writing protein from MAE input file ...")
+        subprocess.run([
+            str(schrodinger_directory / "run"),
+            "delete_atoms.py",
+            str(input_file_mae),
+            protein_file_mae.name,
+            "-asl",
+            "'not protein'"
+        ])
+
+        logger.debug("Converting MAE to PDB ...")
+        input_file_pdb = input_file_mae.parent / (input_file_mae.stem + ".pdb")
+        mae_to_pdb(schrodinger_directory, input_file_mae, input_file_pdb)
+
+        logger.debug("Selecting and writing co-crystallized ligand ...")
+        structure = read_molecule(input_file_mae)
+        ligands = structure.select_atoms(f"resname {ligand_resname}").residues
+        if len(ligands) == 0:
+            logger.debug(
+                f"Could not find ligand {ligand_resname} in structure {input_file_pdb}, "
+                f"cannot proceed to docking!"
+            )
+            return
+        ligands[0].atoms.write(ligand_file_pdb.name)  # write first residue (in case of multiple chains)
+
+        logger.debug("Writing molecules to SDF ...")
+        if not mols_names or len(mols_names) != len(mols_smiles):
+            logger.debug("Creating molecule names ...")
+            mols_names = [str(x) for x in range(1, len(mols_smiles) + 1)]
+        sd_writer = Chem.SDWriter(mols_file_sdf.name)
+        for smiles, name in zip(mols_smiles, mols_names):
+            mol = Chem.MolFromSmiles(smiles)
+            if not mol:
+                logger.debug(f"Skipping molecule {name} with erroneous smiles ...")
+                continue
+            mol.SetProp("_Name", name)
+            mol = Chem.AddHs(mol)
+            AllChem.EmbedMolecule(mol)
+            sd_writer.write(mol)
+
+        logger.debug("Writing input file for grid generation ...")
+        grid_input_file.write(f"RECEP_FILE '{protein_file_mae.name}'\n")
+        grid_input_file.write(f"REF_LIGAND_FILE '{ligand_file_pdb.name}'\n")
+        grid_input_file.write("LIGAND_INDEX 1\n")
+        grid_input_file.flush()
+
+        logger.debug("Generating grid for docking ...")
+        subprocess.run([
+            str(schrodinger_directory / "glide"),
+            grid_input_file.name,
+            "-HOST",
+            "localhost",
+            "-WAIT",
+            "-OVERWRITE"
+        ])
+
+        logger.debug("Writing input file for docking ...")
+        docking_input_file.write(f"GRIDFILE '{grid_input_file.name}'\n")
+        docking_input_file.write(f"LIGANDFILE '{mols_file_sdf.name}'\n")
+        docking_input_file.write("POSE_OUTTYPE ligandlib_sd\n")
+        docking_input_file.write(f"COMPRESS_POSES False\n")
+        docking_input_file.write(f"POSES_PER_LIG {n_poses}\n")
+        docking_input_file.write(f"PRECISION {precision}\n")
+        if shape_restrain:
+            docking_input_file.write(f"SHAPE_RESTRAIN True\n")
+            docking_input_file.write(f"SHAPE_REF_LIGAND_FILE '{ligand_file_pdb.name}'\n")
+        if macrocyles:
+            docking_input_file.write(f"MACROCYCLE True\n")
+        docking_input_file.flush()
+
+        logger.debug("Running docking ...")
+        subprocess.run([
+            str(schrodinger_directory / "glide"),
+            docking_input_file.name,
+            "-HOST",
+            "localhost",
+            "-WAIT",
+            "-OVERWRITE"
+        ])
+
+        logger.debug("Filtering poses for appropriate number ...")
+        grid_input_file_path = Path(grid_input_file.name)
+        sd_file_path = grid_input_file_path.parent / (grid_input_file_path.stem + "_lib.sdf")
+        if not sd_file_path:
+            logger.debug("No docking poses were generated during docking ...")
+            return
+        supplier = Chem.SDMolSupplier(sd_file_path)
+        sd_writer = Chem.SDWriter(output_file_sdf)
+        mol_counter_dict = {}
+        for mol in supplier:
+            # SDF from glide is sorted by docking score, but mols are in mixed order
+            name = mol.GetProp("_Name")
+            if name not in mol_counter_dict.keys():
+                mol_counter_dict[name] = 0
+            if mol_counter_dict[name] <= n_poses:
+                sd_writer.write(mol)
+                mol_counter_dict[name] += 1
+        sd_file_path.unlink()  # manually delete file
+
+    return
--- kinoml/features/schrodinger.py
+++ kinoml/features/schrodinger.py
@@ -183,6 +183,8 @@ class SCHRODINGERComplexFeaturizer(ParallelBaseFeaturizer):
             "protein_alternate_location": None,
             "protein_expo_id": None,
             "ligand_name": None,
+            "ligand_smiles": None,
+            "ligand_macrocycle": False,
         }
 
         logger.debug("Interpreting protein component ...")
@@ -226,6 +228,12 @@ class SCHRODINGERComplexFeaturizer(ParallelBaseFeaturizer):
         logger.debug("Interpreting ligand component ...")
         if hasattr(system.ligand, "name"):
             system_dict["ligand_name"] = system.ligand.name
+
+        if hasattr(system.ligand, "smiles"):
+            system_dict["ligand_smiles"] = system.ligand.smiles
+
+        if hasattr(system.ligand, "macrocycle"):
+            system_dict["ligand_macrocycle"] = system.ligand.macrocycle
 
         return system_dict
 
@@ -385,3 +393,231 @@ class SCHRODINGERComplexFeaturizer(ParallelBaseFeaturizer):
         prepared_structure = update_residue_identifiers(prepared_structure)
 
         return prepared_structure
+
+
+class SCHRODINGERDockingFeaturizer(SCHRODINGERComplexFeaturizer):
+    """
+    Given systems with exactly one protein and one ligand dock the ligand into the protein
+    structure. The protein structure needs to have a co-crystallized ligand to identify the
+    pocket for docking. The following steps will be performed.
+
+     - modeling missing loops
+     - building missing side chains
+     - mutations, if `uniprot_id` or `sequence` attribute is provided for the protein component
+       (see below)
+     - removing everything but protein, water and ligand of interest
+     - protonation at pH 7.4
+     - docking a ligand
+
+    The protein component of each system must have a `pdb_id` or a `path` attribute specifying
+    the complex structure to prepare.
+
+     - `pdb_id`: A string specifying the PDB entry of interest, required if `path` not given.
+     - `path`: The path to the structure file, required if `pdb_id` not given.
+
+    Additionally, the protein component can have the following optional attributes to customize
+    the protein modeling:
+     - `name`: A string specifying the name of the protein, will be used for generating the
+       output file name.
+     - `chain_id`: A string specifying which chain should be used.
+     - `alternate_location`: A string specifying which alternate location should be used.
+     - `expo_id`: A string specifying the ligand of interest. This is especially useful if
+       multiple ligands are present in a PDB structure.
+     - `uniprot_id`: A string specifying the UniProt ID that will be used to fetch the amino acid
+       sequence from UniProt, which will be used for modeling the protein. This will supersede the
+       sequence information given in the PDB header.
+     - `sequence`: A string specifying the amino acid sequence in single letter codes to be used
+       during loop modeling and for mutations.
+
+    The ligand component should be a BaseLigand with smiles and name attribute:
+
+     - `name`: A string specifying the name of the ligand, will be used for generating the
+       output file name.
+     - `smiles`: A SMILES representation of the molecule to dock.
+
+    Parameters
+    ----------
+    cache_dir: str, Path or None, default=None
+        Path to directory used for saving intermediate files. If None, default location
+        provided by `appdirs.user_cache_dir()` will be used.
+    output_dir: str, Path or None, default=None
+        Path to directory used for saving output files. If None, output structures will not be
+        saved.
+    shape_restrain: bool, default=True
+        If the docking shell be performed with shape restrain based on the co-crystallized
+        ligand.
+    max_retry: int, default=3
+        The maximal number of attempts to try running the prepwizard step.
+    """
+    from MDAnalysis.core.universe import Universe
+
+    def __init__(
+            self,
+            cache_dir: Union[str, Path, None] = None,
+            output_dir: Union[str, Path, None] = None,
+            shape_restrain: bool = True,
+            max_retry: int = 3,
+            **kwargs,
+    ):
+        from appdirs import user_cache_dir
+
+        super().__init__(**kwargs)
+        self.cache_dir = Path(user_cache_dir())
+        if cache_dir:
+            self.cache_dir = Path(cache_dir).expanduser().resolve()
+        self.cache_dir.mkdir(parents=True, exist_ok=True)
+        if output_dir:
+            self.output_dir = Path(output_dir).expanduser().resolve()
+            self.output_dir.mkdir(parents=True, exist_ok=True)
+            self.save_output = True
+        else:
+            self.output_dir = Path(user_cache_dir())
+            self.save_output = False
+        self.shape_restrain = shape_restrain
+        self.max_retry = max_retry
+
+    _SUPPORTED_TYPES = (ProteinLigandComplex,)
+
+    def _featurize_one(self, system: ProteinLigandComplex) -> Union[Universe, None]:
+        """
+        Prepare a protein structure and dock a ligand.
+
+        Parameters
+        ----------
+        system: ProteinLigandComplex
+            A system object holding a protein and a ligand component.
+
+        Returns
+        -------
+        : Universe or None
+            An MDAnalysis universe of the featurized system or None if not successful.
+        """
+        from ..modeling.MDAnalysisModeling import write_molecule
+        from ..utils import LocalFileStorage
+
+        logger.debug("Interpreting system ...")
+        system_dict = self._interpret_system(system)
+
+        if not system_dict["protein_expo_id"]:
+            logger.debug(
+                "No expo_id given in Protein object needed for docking, returning None!"
+            )
+            return None
+
+        if system_dict["protein_sequence"]:
+            system_dict["protein_path"] = self._preprocess_structure(
+                pdb_path=system_dict["protein_path"],
+                chain_id=system_dict["protein_chain_id"],
+                alternate_location=system_dict["protein_alternate_location"],
+                expo_id=system_dict["protein_expo_id"],
+                sequence=system_dict["protein_sequence"],
+            )
+
+        prepared_structure_path = self._prepare_structure(
+            system_dict["protein_path"], system_dict["protein_sequence"]
+        )
+        if not prepared_structure_path.is_file():
+            return None
+
+        docking_pose_path = LocalFileStorage.featurizer_result(
+            self.__class__.__name__,
+            f"{system_dict['protein_name']}_{system_dict['ligand_name']}_complex",
+            "sdf",
+            self.output_dir,
+        )
+        mae_file_path = prepared_structure_path.parent / f"{prepared_structure_path.stem}.mae"
+        if not self._dock_molecule(
+            mae_file=mae_file_path,
+            output_file_sdf=docking_pose_path,
+            ligand_resname=system_dict["protein_expo_id"],
+            smiles=system_dict["ligand_smiles"],
+            macrocycle=system_dict["ligand_macrocycle"],
+        ):
+            logger.debug("Failed to generate docking pose ...")
+            return None
+
+        self._replace_ligand(
+            pdb_path=prepared_structure_path,
+            resname_replace=system_dict["protein_expo_id"],
+            docking_pose_sdf_path=docking_pose_path
+        )
+
+        prepared_structure = self._postprocess_structure(
+            pdb_path=prepared_structure_path,
+            chain_id=system_dict["protein_chain_id"],
+            alternate_location=system_dict["protein_alternate_location"],
+            expo_id=["LIG"],
+            sequence=system_dict["protein_sequence"],
+        )
+
+        if self.save_output:
+            logging.debug("Saving results ...")
+            complex_path = LocalFileStorage.featurizer_result(
+                self.__class__.__name__,
+                f"{system_dict['protein_name']}_{system_dict['ligand_name']}_complex",
+                "pdb",
+                self.output_dir,
+            )
+            write_molecule(prepared_structure.atoms, complex_path)
+
+        return prepared_structure
+
+    def _dock_molecule(self, mae_file, output_file_sdf, ligand_resname, smiles, macrocycle):
+
+        from ..docking.SCHRODINGERDocking import run_glide
+
+        run_glide(
+            schrodinger_directory=self.schrodinger,
+            input_file_mae=mae_file,
+            output_file_sdf=output_file_sdf,
+            ligand_resname=ligand_resname,
+            mols_smiles=[smiles],
+            mols_names=["LIG"],
+            n_poses=1,
+            shape_restrain=self.shape_restrain,
+            macrocyles=macrocycle,
+            precision="XP"
+        )
+
+        if not output_file_sdf.is_file():
+            return False
+
+        return True
+
+    @staticmethod
+    def _replace_ligand(pdb_path, resname_replace, docking_pose_sdf_path):
+        from tempfile import NamedTemporaryFile
+
+        from MDAnalysis.core.universe import Merge
+        from rdkit import Chem
+
+        from ..modeling.MDAnalysisModeling import read_molecule, write_molecule, delete_residues
+
+        logger.debug("Removing co-crystallized ligand ...")
+        prepared_structure = read_molecule(pdb_path)
+        chain_id = prepared_structure.select_atoms(f"resname {resname_replace}").residues[0].segid
+        prepared_structure = prepared_structure.select_atoms(f"not resname {resname_replace}")
+
+        with NamedTemporaryFile(mode="w", suffix=".pdb") as docking_pose_pdb_path:
+            logger.debug("Converting docking pose SDF to PDB ...")
+            mol = next(Chem.SDMolSupplier(docking_pose_sdf_path, removeHs=False))
+            Chem.MolToPDBFile(mol, docking_pose_pdb_path.name)
+
+            logger.debug("Readind docking pose and renaming residue ...")
+            docking_pose = read_molecule(docking_pose_pdb_path.name)
+            for atom in docking_pose.atoms:
+                atom.residue.resname = "LIG"
+                atom.segment.segid = chain_id
+
+            logger.debug("Adding docking pose to structure ...")
+            prepared_structure = Merge(prepared_structure, docking_pose.atoms)
+
+            logger.debug("Deleting water clashing with docking pose ...")
+            clashing_water = prepared_structure.select_atoms(
+                "(resname HOH and element O) and around 1.5 resname LIG"
+            )
+            prepared_structure = delete_residues(prepared_structure, clashing_water)
+
+        write_molecule(prepared_structure.atoms, pdb_path)
+
+        return
