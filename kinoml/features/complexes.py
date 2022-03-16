adb8f6fb9793cff77bcc9a3fdb421149c624d080
David Schaller <schaller.david@yahoo.de>
2022-03-16 15:42:42 +0100

    moving fred

--- kinoml/features/complexes.py
+++ kinoml/features/complexes.py
@@ -193,192 +193,6 @@ class OEComplexFeaturizer(OEBaseModelingFeaturizer, SingleLigandProteinComplexFe
         return structure
 
 
-class OEHybridDockingFeaturizer(OEBaseModelingFeaturizer, SingleLigandProteinComplexFeaturizer):
-    """
-    Given systems with exactly one protein and one ligand, dock the ligand into the prepared
-    protein structure with OpenEye's Hybrid method:
-
-     - modeling missing loops
-     - building missing side chains
-     - mutations, if `uniprot_id` or `sequence` attribute is provided for the protein component
-       (see below)
-     - removing everything but protein, water and ligand of interest
-     - protonation at pH 7.4
-     - perform Hybrid docking
-
-    The protein component of each system must have a `pdb_id` or a `path` attribute specifying
-    the complex structure to prepare.
-
-     - `pdb_id`: A string specifying the PDB entry of interest, required if `path` not given.
-     - `path`: The path to the structure file, required if `pdb_id` not given.
-
-    Additionally, the protein component can have the following optional attributes to customize
-    the protein modeling:
-
-     - `name`: A string specifying the name of the protein, will be used for generating the
-       output file name.
-     - `chain_id`: A string specifying which chain should be used.
-     - `alternate_location`: A string specifying which alternate location should be used.
-     - `expo_id`: A string specifying the ligand to use during Hybrid docking. This is especially
-       useful if multiple ligands are present in a PDB structure.
-     - `uniprot_id`: A string specifying the UniProt ID that will be used to fetch the amino acid
-       sequence from UniProt, which will be used for modeling the protein. This will supersede the
-       sequence information given in the PDB header.
-     - `sequence`: An `AminoAcidSequence` object specifying the amino acid sequence that should be
-       used during modeling the protein. This will supersede a given `uniprot_id` and the sequence
-       information given in the PDB header.
-
-    The ligand component of each system must have a `to_smiles` method allowing access to the
-    molecular structure to dock, e.g. a Ligand object from `core.ligands` initiated as
-    Ligand.from_smiles("CCOCCC").
-
-    Additionally, the ligand component can have the following optional attributes:
-
-     - `name`: A string specifying the name of the ligand, will be used for generating the
-       output file name.
-
-    Parameters
-    ----------
-    loop_db: str
-        The path to the loop database used by OESpruce to model missing loops.
-    cache_dir: str, Path or None, default=None
-        Path to directory used for saving intermediate files. If None, default location
-        provided by `appdirs.user_cache_dir()` will be used.
-    output_dir: str, Path or None, default=None
-        Path to directory used for saving output files. If None, output structures will not be
-        saved.
-    pKa_norm: bool, default=True
-        Assign the predominant ionization state of the molecules to dock at pH ~7.4. If False,
-        the ionization state of the input molecules will be conserved.
-
-    Note
-    ----
-    The provided protein must have a co-crystallized ligand to allow docking with OpenEye's
-    Hybrid method.
-    """
-    from MDAnalysis.core.universe import Universe
-
-    def __init__(self, pKa_norm: bool = True, **kwargs):
-        super().__init__(**kwargs)
-        self.pKa_norm = pKa_norm
-
-    _SUPPORTED_TYPES = (ProteinLigandComplex,)
-
-    def _featurize_one(self, system: ProteinLigandComplex) -> Union[Universe, None]:
-        """
-        Prepare a protein structure and dock a ligand using OpenEye's Hybrid method.
-
-        Parameters
-        ----------
-        system: ProteinLigandComplex
-            A system object holding a protein and a ligand component.
-
-        Returns
-        -------
-        : Universe or None
-            An MDAnalysis universe of the featurized system. None if no design unit or docking
-            pose was found.
-        """
-        from pathlib import Path
-
-        import MDAnalysis as mda
-        from openeye import oechem, oedocking
-
-        from ..docking.OEDocking import hybrid_docking
-
-        structure = self._read_protein_structure(system.protein)
-        if structure is None:
-            logger.warning(
-                f"Could not read protein structure for {system.protein}, returning None!"
-            )
-            return None
-
-        logger.debug("Preparing protein ligand complex ...")
-        design_unit = self._get_design_unit(
-            structure=structure,
-            chain_id=system.protein.chain_id if hasattr(system.protein, "chain_id") else None,
-            alternate_location=system.protein.alternate_location if hasattr(
-                system.protein, "alternate_location"
-            ) else None,
-            has_ligand=True,
-            ligand_name=system.protein.expo_id if hasattr(system.protein, "expo_id") else None,
-            model_loops_and_caps=False if system.protein.sequence else True,
-        )  # if sequence is given model loops and caps separately later
-        if not design_unit:
-            logger.debug("No design unit found, returning None!")
-            return None
-
-        logger.debug("Extracting design unit components ...")
-        protein, solvent, ligand = self._get_components(
-            design_unit=design_unit,
-            chain_id=system.protein.chain_id if hasattr(system.protein, "chain_id") else None
-        )
-
-        if system.protein.sequence:
-            first_id = 1
-            if "construct_range" in system.protein.metadata.keys():
-                first_id = int(system.protein.metadata["construct_range"].split("-")[0])
-            protein = self._process_protein(
-                protein_structure=protein,
-                amino_acid_sequence=system.protein.sequence,
-                first_id=first_id,
-                ligand=ligand,
-            )
-            oechem.OEUpdateDesignUnit(design_unit, protein, oechem.OEDesignUnitComponents_Protein)
-
-        if not design_unit.HasReceptor():
-            logger.debug("Preparing receptor for docking ...")
-            oedocking.OEMakeReceptor(design_unit)
-
-        logger.debug("Performing docking ...")
-        docking_poses = hybrid_docking(
-            design_unit,
-            [system.ligand.molecule.to_openeye()],
-            pKa_norm=self.pKa_norm
-        )
-        if not docking_poses:
-            logger.debug("No docking pose found, returning None!")
-            return None
-        else:
-            docking_pose = docking_poses[0]
-        # generate residue information
-        oechem.OEPerceiveResidues(docking_pose, oechem.OEPreserveResInfo_None)
-
-        logger.debug("Assembling components ...")
-        protein_ligand_complex = self._assemble_components(protein, solvent, docking_pose)
-
-        logger.debug("Updating pdb header ...")
-        protein_ligand_complex = self._update_pdb_header(
-            protein_ligand_complex,
-            protein_name=system.protein.name,
-            ligand_name=system.ligand.name,
-        )
-
-        logger.debug("Writing results ...")
-        file_path = self._write_results(
-            protein_ligand_complex,
-            "_".join([info for info in [
-                system.protein.name,
-                system.protein.pdb_id if system.protein.pdb_id
-                else Path(system.protein.metadata["file_path"]).stem,
-                f"chain{system.protein.chain_id}" if hasattr(system.protein, "chain_id")
-                else None,
-                f"altloc{system.protein.alternate_location}"
-                if hasattr(system.protein, "alternate_location") else None,
-            ] if info]),
-            system.ligand.name,
-        )
-
-        logger.debug("Generating new MDAnalysis universe ...")
-        structure = mda.Universe(file_path, in_memory=True)
-
-        if not self.output_dir:
-            logger.debug("Removing structure file ...")
-            file_path.unlink()
-
-        return structure
-
-
 class OEFredDockingFeaturizer(OEBaseModelingFeaturizer, SingleLigandProteinComplexFeaturizer):
     """
     Given systems with exactly one protein and one ligand, dock the ligand into the prepared
@@ -575,6 +389,192 @@ class OEFredDockingFeaturizer(OEBaseModelingFeaturizer, SingleLigandProteinCompl
         return structure
 
 
+class OEHybridDockingFeaturizer(OEBaseModelingFeaturizer, SingleLigandProteinComplexFeaturizer):
+    """
+    Given systems with exactly one protein and one ligand, dock the ligand into the prepared
+    protein structure with OpenEye's Hybrid method:
+
+     - modeling missing loops
+     - building missing side chains
+     - mutations, if `uniprot_id` or `sequence` attribute is provided for the protein component
+       (see below)
+     - removing everything but protein, water and ligand of interest
+     - protonation at pH 7.4
+     - perform Hybrid docking
+
+    The protein component of each system must have a `pdb_id` or a `path` attribute specifying
+    the complex structure to prepare.
+
+     - `pdb_id`: A string specifying the PDB entry of interest, required if `path` not given.
+     - `path`: The path to the structure file, required if `pdb_id` not given.
+
+    Additionally, the protein component can have the following optional attributes to customize
+    the protein modeling:
+
+     - `name`: A string specifying the name of the protein, will be used for generating the
+       output file name.
+     - `chain_id`: A string specifying which chain should be used.
+     - `alternate_location`: A string specifying which alternate location should be used.
+     - `expo_id`: A string specifying the ligand to use during Hybrid docking. This is especially
+       useful if multiple ligands are present in a PDB structure.
+     - `uniprot_id`: A string specifying the UniProt ID that will be used to fetch the amino acid
+       sequence from UniProt, which will be used for modeling the protein. This will supersede the
+       sequence information given in the PDB header.
+     - `sequence`: An `AminoAcidSequence` object specifying the amino acid sequence that should be
+       used during modeling the protein. This will supersede a given `uniprot_id` and the sequence
+       information given in the PDB header.
+
+    The ligand component of each system must have a `to_smiles` method allowing access to the
+    molecular structure to dock, e.g. a Ligand object from `core.ligands` initiated as
+    Ligand.from_smiles("CCOCCC").
+
+    Additionally, the ligand component can have the following optional attributes:
+
+     - `name`: A string specifying the name of the ligand, will be used for generating the
+       output file name.
+
+    Parameters
+    ----------
+    loop_db: str
+        The path to the loop database used by OESpruce to model missing loops.
+    cache_dir: str, Path or None, default=None
+        Path to directory used for saving intermediate files. If None, default location
+        provided by `appdirs.user_cache_dir()` will be used.
+    output_dir: str, Path or None, default=None
+        Path to directory used for saving output files. If None, output structures will not be
+        saved.
+    pKa_norm: bool, default=True
+        Assign the predominant ionization state of the molecules to dock at pH ~7.4. If False,
+        the ionization state of the input molecules will be conserved.
+
+    Note
+    ----
+    The provided protein must have a co-crystallized ligand to allow docking with OpenEye's
+    Hybrid method.
+    """
+    from MDAnalysis.core.universe import Universe
+
+    def __init__(self, pKa_norm: bool = True, **kwargs):
+        super().__init__(**kwargs)
+        self.pKa_norm = pKa_norm
+
+    _SUPPORTED_TYPES = (ProteinLigandComplex,)
+
+    def _featurize_one(self, system: ProteinLigandComplex) -> Union[Universe, None]:
+        """
+        Prepare a protein structure and dock a ligand using OpenEye's Hybrid method.
+
+        Parameters
+        ----------
+        system: ProteinLigandComplex
+            A system object holding a protein and a ligand component.
+
+        Returns
+        -------
+        : Universe or None
+            An MDAnalysis universe of the featurized system. None if no design unit or docking
+            pose was found.
+        """
+        from pathlib import Path
+
+        import MDAnalysis as mda
+        from openeye import oechem, oedocking
+
+        from ..docking.OEDocking import hybrid_docking
+
+        structure = self._read_protein_structure(system.protein)
+        if structure is None:
+            logger.warning(
+                f"Could not read protein structure for {system.protein}, returning None!"
+            )
+            return None
+
+        logger.debug("Preparing protein ligand complex ...")
+        design_unit = self._get_design_unit(
+            structure=structure,
+            chain_id=system.protein.chain_id if hasattr(system.protein, "chain_id") else None,
+            alternate_location=system.protein.alternate_location if hasattr(
+                system.protein, "alternate_location"
+            ) else None,
+            has_ligand=True,
+            ligand_name=system.protein.expo_id if hasattr(system.protein, "expo_id") else None,
+            model_loops_and_caps=False if system.protein.sequence else True,
+        )  # if sequence is given model loops and caps separately later
+        if not design_unit:
+            logger.debug("No design unit found, returning None!")
+            return None
+
+        logger.debug("Extracting design unit components ...")
+        protein, solvent, ligand = self._get_components(
+            design_unit=design_unit,
+            chain_id=system.protein.chain_id if hasattr(system.protein, "chain_id") else None
+        )
+
+        if system.protein.sequence:
+            first_id = 1
+            if "construct_range" in system.protein.metadata.keys():
+                first_id = int(system.protein.metadata["construct_range"].split("-")[0])
+            protein = self._process_protein(
+                protein_structure=protein,
+                amino_acid_sequence=system.protein.sequence,
+                first_id=first_id,
+                ligand=ligand,
+            )
+            oechem.OEUpdateDesignUnit(design_unit, protein, oechem.OEDesignUnitComponents_Protein)
+
+        if not design_unit.HasReceptor():
+            logger.debug("Preparing receptor for docking ...")
+            oedocking.OEMakeReceptor(design_unit)
+
+        logger.debug("Performing docking ...")
+        docking_poses = hybrid_docking(
+            design_unit,
+            [system.ligand.molecule.to_openeye()],
+            pKa_norm=self.pKa_norm
+        )
+        if not docking_poses:
+            logger.debug("No docking pose found, returning None!")
+            return None
+        else:
+            docking_pose = docking_poses[0]
+        # generate residue information
+        oechem.OEPerceiveResidues(docking_pose, oechem.OEPreserveResInfo_None)
+
+        logger.debug("Assembling components ...")
+        protein_ligand_complex = self._assemble_components(protein, solvent, docking_pose)
+
+        logger.debug("Updating pdb header ...")
+        protein_ligand_complex = self._update_pdb_header(
+            protein_ligand_complex,
+            protein_name=system.protein.name,
+            ligand_name=system.ligand.name,
+        )
+
+        logger.debug("Writing results ...")
+        file_path = self._write_results(
+            protein_ligand_complex,
+            "_".join([info for info in [
+                system.protein.name,
+                system.protein.pdb_id if system.protein.pdb_id
+                else Path(system.protein.metadata["file_path"]).stem,
+                f"chain{system.protein.chain_id}" if hasattr(system.protein, "chain_id")
+                else None,
+                f"altloc{system.protein.alternate_location}"
+                if hasattr(system.protein, "alternate_location") else None,
+            ] if info]),
+            system.ligand.name,
+        )
+
+        logger.debug("Generating new MDAnalysis universe ...")
+        structure = mda.Universe(file_path, in_memory=True)
+
+        if not self.output_dir:
+            logger.debug("Removing structure file ...")
+            file_path.unlink()
+
+        return structure
+
+
 class OEPositDockingFeaturizer(OEBaseModelingFeaturizer, SingleLigandProteinComplexFeaturizer):
     """
     Given systems with exactly one protein and one ligand, dock the ligand into the prepared
@@ -614,28 +614,10 @@ class OEPositDockingFeaturizer(OEBaseModelingFeaturizer, SingleLigandProteinComp
     molecular structure to dock, e.g. a Ligand object from `core.ligands` initiated as
     Ligand.from_smiles("CCOCCC").
 
-    Additionally, the ligand component can have the following optional attributes. The
-    docking_template attributes can be used to transfer a ligand from another protein:ligand
-    complex to bias the docking with Posit into the protein structure specified via the `path` or
-    `pdb_id` attributes listed above. The docking template will thereby be superposed to the
-    protein structure. Hence, they need to have a sufficient sequence similarity to allow a proper
-    superposition.
+    Additionally, the ligand component can have the following optional attributes.
 
      - `name`: A string specifying the name of the ligand, will be used for generating the
        output file name.
-     - `docking_template_pdb_id` - A string specifying a PDB entry with a co-crystallized ligand
-       that should be used to bias the docking with Posit into the protein structure specified
-       above.
-     - `docking_template_path` - The path to a structure file with a co-crystallized ligand that
-       should be used to bias the docking with Posit into the protein structure specified above.
-     - `docking_template_expo_id` - A string specifying the ligand to transfer to bias Posit
-       docking. This is especially useful if multiple ligands are present in a PDB structure.
-     - `docking_template_chain_id` - A string specifying which chain should be used. This is
-       especially useful if multiple chains are present in the given PDB structure. If not given
-       this may lead to unexpected behavior during superposition of the docking_template to the
-       actual protein structure.
-     - `docking_template_alternate_location` - A string specifying which alternate location should
-       be used.
 
     Parameters
     ----------
@@ -685,11 +667,6 @@ class OEPositDockingFeaturizer(OEBaseModelingFeaturizer, SingleLigandProteinComp
         from openeye import oechem, oedocking
 
         from ..docking.OEDocking import pose_molecules
-        from ..modeling.OEModeling import (
-            read_smiles,
-            select_chain,
-            superpose_proteins
-        )
 
         structure = self._read_protein_structure(system.protein)
         if structure is None:
